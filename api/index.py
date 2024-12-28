024a157a80570387008b411efa4b11ca7d2600ce
dedsec <jackiehansel@protonmail.com>
2024-12-28 19:01:08 +0530

    more vercel testing2

--- api/index.py
+++ api/index.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template_string
-from test import return_int
+from flask import Flask, render_template
+import httpx
+import re
+
+
+client = httpx.Client(headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0'})
 
 app = Flask(__name__)
 
@@ -13,5 +17,9 @@ def about():
 
 @app.route('/random_number')
 def random_number():
-    html = f'<p>{return_int()}</p>'
-    return render_template_string(html)
+    r = client.get("https://popcornmovies.to/search/mr. robot")
+    data = re.findall(r'<a href="(.*?)"\s+class=".*?">\s+<picture>\s+<img src=".*?" data-src="(.*?)"',r.text)[0]
+
+    return render_template('index.html',url=data[0],image=data[1])
+
+
--- api/test.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def return_int():
-    return random.randint(100,200)
