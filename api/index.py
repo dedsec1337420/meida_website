e8061bf06136a11845107d8ca7fdbe94d10d2549
dedsec <jackiehansel@protonmail.com>
2024-12-29 19:38:49 +0530

    Movie and tv Search implemented

--- api/index.py
+++ api/index.py
@@ -1,25 +1,56 @@
 from flask import Flask, render_template
+from flask import request
 import httpx
 import re
 
+#global shit
+USER_AGENT:str = 'Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0'
+POP_MAIN_URL:str ="https://popcornmovies.to"
+ANIME_URL:str =""
+ANIME_REF:str = ""
 
-client = httpx.Client(headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0'})
+
+
+#search functions
+def pop_search(media_type:str,query:str)->list:
+    r=httpx.get(f"{POP_MAIN_URL}/search/{query.replace(" ", "%20")}",headers={'User-Agent': USER_AGENT})
+
+    if r.status_code == 200:
+        results = re.findall(r'<div class=\"relative group overflow-hidden\">\s+<a href=\"(.*?)\"\s+class=".*?">\s+<picture>\s+<img .+? data-src="(.*?)" alt="(.*?)"',r.text)
+
+        if media_type == "movie":
+            search_result= [r for r in results if "/movie/" in r[0]]
+        else:
+            search_result = [r for r in results if "/tv-show/" in r[0]]
+
+        return search_result if len(search_result) > 0 else None
+    else:
+        return None
+
+def anime_search(query:str):
+    pass
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/search', methods=['POST'])
+def search():
+    media_type = request.form.get('media_type')
+    query = request.form.get('query')
+    #return f"Media Type: {media_type}, Query: {query}"
+    if media_type == "movie" or media_type == "tv":
+        search_result = pop_search(media_type=media_type,query=query)
+        if search_result:
+            return render_template('result.html',results=search_result,media_type=media_type)
+            
+        else:
+            return "<p>Sorry no data found</p>"
 
-@app.route('/random_number')
-def random_number():
-    r = client.get("https://popcornmovies.to/search/mr. robot")
-    data = re.findall(r'<a href="(.*?)"\s+class=".*?">\s+<picture>\s+<img src=".*?" data-src="(.*?)"',r.text)[0]
 
-    return render_template('index.html',url=data[0],image=data[1])
-
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0',port=8888,debug=True)
 
